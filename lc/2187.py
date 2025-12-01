210865b31347e0941c08c8bacbbec989731e7bcc
Amar Vashishth <vashishth.amar@gmail.com>
2025-12-02 00:00:33 +0900

    feat: incorrect push

--- lc/2187.py
+++ lc/2187.py
@@ -2,13 +2,23 @@ from typing import List
 
 
 class Solution:
+    def willComplete(self, time, mid, totalTrips):
+        ans = 0
+        for t in time:
+            ans += mid // t
+        return ans >= totalTrips
+
     def minimumTime(self, time: List[int], totalTrips: int) -> int:
         left = min(time) * totalTrips
         right = max(time) * totalTrips
         ans = left
         while left < right:
             mid = left + (right - left) // 2
-            
+            if self.willComplete(time, mid, totalTrips):
+                right = mid
+            else:
+                left = mid + 1
+        return left
 
 
 time = [1, 2, 3]
