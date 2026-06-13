537c70a3282452e4110d756791e247b85aad4b89
Amar Vashishth <vashishth.amar@gmail.com>
2026-06-13 18:51:27 +0900

    rev: stack

--- revision/june-12/stack.py
+++ revision/june-12/stack.py
@@ -3,21 +3,33 @@ from typing import List
 
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        ans = [0] * len(temperatures)
+        n = len(temperatures)
+        ans = [0 for _ in range(n)]
         stk = []
-        for i in range(len(temperatures)):
+        for i in range(n):
             while stk and temperatures[i] > temperatures[stk[-1]]:
                 index = stk.pop()
                 ans[index] = i - index
             stk.append(i)
         return ans
 
+    def largestRectangleArea(self, heights):
+        stk = [0]
+        heights.append(0)
+        ans = 0
+        for i in range(len(heights)):
+            while stk and heights[i] < heights[stk[-1]]:
+                ht = heights[stk.pop()]
+                if not stk:
+                    wd = i
+                else:
+                    wd = i - stk[-1] - 1
+                ans = max(ans, ht*wd)
+            stk.append(i)
+        return ans
 
-temperatures = [73, 74, 75, 71, 69, 72, 76, 73]
-print(Solution().dailyTemperatures(temperatures))
+heights = [2,1,5,6,2,3]
+print(Solution().largestRectangleArea(heights))
 
-temperatures = [30, 40, 50, 60]
-print(Solution().dailyTemperatures(temperatures))
-
-temperatures = [30, 60, 90]
-print(Solution().dailyTemperatures(temperatures))
+heights = [2,4]
+print(Solution().largestRectangleArea(heights))
