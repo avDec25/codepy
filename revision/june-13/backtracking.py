eeeb9e84250f7c1876e64c9bbc9918d385b1da7b
Amar Vashishth <vashishth.amar@gmail.com>
2026-06-15 13:53:44 +0900

    rev: backtrack

--- revision/june-13/backtracking.py
+++ revision/june-13/backtracking.py
@@ -64,20 +64,20 @@ class Solution:
         board = [['.' for _ in range(n)] for _ in range(n)]
 
         def is_safe(r, c):
-            for j in range(c-1, -1, -1):
+            for j in range(c - 1, -1, -1):
                 if board[r][j] == 'Q':
                     return False
 
-            i = r-1
-            j = c-1
+            i = r - 1
+            j = c - 1
             while 0 <= i and 0 <= j:
                 if board[i][j] == 'Q':
                     return False
                 i -= 1
                 j -= 1
 
-            i = r+1
-            j = c-1
+            i = r + 1
+            j = c - 1
             while i < n and 0 <= j:
                 if board[i][j] == 'Q':
                     return False
@@ -100,6 +100,47 @@ class Solution:
         backtrack(0)
         return ans
 
+    def solveSudoku(self, board: List[List[str]]) -> None:
+        row = len(board)
+        col = len(board[0])
 
-n = 4
-print(Solution().solveNQueens(n))
+        if board is None or row == 0 or col == 0:
+            return
+
+        def isValid(r, c, x):
+            for i in range(9):
+                if board[r][i] == x:
+                    return False
+                if board[i][c] == x:
+                    return False
+                if board[3 * (r // 3) + i // 3][3 * (c // 3) + i % 3] == x:
+                    return False
+            return True
+
+        def solve(board):
+            for r in range(9):
+                for c in range(9):
+                    if board[r][c] == '.':
+                        for x in ['1', '2', '3', '4', '5', '6', '7', '8', '9']:
+                            if isValid(r, c, x):
+                                board[r][c] = x
+                                if solve(board): return True
+                                board[r][c] = '.'
+                        return False
+            return True
+
+        solve(board)
+
+
+board = [["5", "3", ".", ".", "7", ".", ".", ".", "."],
+         ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+         [".", "9", "8", ".", ".", ".", ".", "6", "."],
+         ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+         ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+         ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+         [".", "6", ".", ".", ".", ".", "2", "8", "."],
+         [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+         [".", ".", ".", ".", "8", ".", ".", "7", "9"]]
+Solution().solveSudoku(board)
+for row in board:
+    print(row)
