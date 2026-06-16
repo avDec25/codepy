b5de53ad8675efb963510ac710f22feab12ba01b
Amar Vashishth <vashishth.amar@gmail.com>
2026-06-16 23:43:42 +0900

    rev: dynamic programming

--- revision/june-15/dynamic-programming.py
+++ revision/june-15/dynamic-programming.py
@@ -1,4 +1,11 @@
-from typing import List
+from typing import List, Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
 
 class Solution:
@@ -97,7 +104,19 @@ class Solution:
         nif = solve(0, nums[1:])
         return max(nil, nif)
 
-    def rob3(self, nums: List[int]) -> int:
+    def rob(self, root: Optional[TreeNode]) -> int:
+        def f(node):
+            if not node:
+                return 0, 0
+
+            left = f(node.left)
+            right = f(node.right)
+            rob_this = node.val + left[1] + right[1]
+            not_rob_this = max(left) + max(right)
+
+            return rob_this, not_rob_this
+
+        return max(f(root))
 
 
 nums = [2, 3, 2]
